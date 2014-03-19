b6eea524905f8f11437c74a1d8d2cd242b2b5fbd
Andreas Stuhlmueller <andreas@stuhlmueller.org>
2014-03-19 10:47:27 -0700

    Added fuzzy autocomplete search and fast navigation.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,5 @@
+// Github repository links and buttons
+
 github_repository = "https://github.com/forestdb/forestdb.org/"
 
 function github_edit_url(page_url) {
@@ -15,3 +17,47 @@ function github_page_url(page_url) {
        return github_repository + "blob/gh-pages" + page_url.slice(0, -4) + "md";
    };
 }
+
+
+// Fuzzy autocomplete search + fast navigation
+
+var substringMatcher = function(strs) {
+  return function findMatches(q, cb) {
+    var matches, substringRegex;
+    matches = [];
+    q = q.split("").reduce(function(a,b){ return a+'[^'+b+']*'+b; }); 
+    substrRegex = new RegExp(q, 'i');
+    $.each(strs, function(i, str) {
+      if (substrRegex.test(str)) {
+        if (matches.length < 10) {
+            matches.push({ value: str });
+        };
+      };
+    });
+    cb(matches);
+  };
+};
+
+$(function () {
+  $('#cse-text').typeahead({
+    hint: true,
+    highlight: true,
+    minLength: 1
+  },
+  {
+    name: 'model_names',
+    displayKey: 'value',
+    source: substringMatcher(model_names)
+  });
+  $('#cse-text').bind('typeahead:selected', function(obj, datum, name) {
+      console.log(datum.value);
+      for (var i = 0; i < model_names.length; i++) {
+          if (model_names[i] == datum.value) {
+              document.location.href = model_urls[i];
+              return true;
+          };
+      };
+      console.log("not found");
+      return true;
+  });
+})
